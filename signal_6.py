5bda672f9b140bfda58b453a1642c1e81d25e7ed
sebaf96 <sefernandez96@gmail.com>
2020-04-23 12:20:08 -0300

    ejercicio 6 parece terminado. funciona bien cuando mandamos sigint y cuando lo dejamos enviar las 10 señales

--- signal_6.py
+++ signal_6.py
@@ -5,24 +5,25 @@ import signal
 import time
 
 
-def handler(s, frame):
-    if s == signal.SIGUSR1:
-        print("Child proccess here, signal", s, "received")
-    if s == 2:
-        print("Parent received SIGINT, finishing now...")
+def handler(signum, frame):
+    if signum == signal.SIGUSR1:
+        print("Child process here, signal", signum, "received")
+    if signum == signal.SIGINT:
+        print("Parent received SIGINT, killing child and finishing now...")
         os.kill(-os.getpid(), signal.SIGTERM)
+        # I'm using the NEGATIVE father's pid to send the same signal to the children process.
+        # This way, the parent will "kill -TERM" his child and himself.
 
 
 def parent(child_pid):
     for i in range(10):
         os.kill(child_pid, signal.SIGUSR1)
-        time.sleep(5)
+        time.sleep(5) if i < 9 else time.sleep(1)
     os.kill(child_pid, signal.SIGTERM)
 
 
 def main():
     child_pid = os.fork()
-
     signal.signal(signal.SIGUSR1, handler) if child_pid == 0 else signal.signal(signal.SIGINT, handler)
 
     if child_pid == 0:
