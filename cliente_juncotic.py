d9abb27a26aca148467922fd12c79cd591b8c24a
sebaf96 <sefernandez96@gmail.com>
2020-05-28 18:56:27 -0300

    Agregado diccionario de response codes para checkear input de cliente, cambiado nombre a archivo

--- cliente_juncotic.py
+++ cliente_juncotic.py
@@ -4,13 +4,16 @@ import socket
 import sys
 import getopt
 
+RESPONSE_CODES = {200: 'OK', 400: 'The command seems valid but out of place', 404: 'Wrong Key', 405: 'Null String',
+                  500: 'Invalid command'}
+
 
 def communicate(connection, message):
     connection.send(message.encode('ascii'))
-    response = connection.recv(1024).decode("ascii")
-    print("Response from server:", response)
+    response_code = int(connection.recv(1024).decode("ascii"))
+    print("Response from server:", response_code, '-', RESPONSE_CODES.get(response_code), '\n')
 
-    return int(response)
+    return int(response_code)
 
 
 def main():
@@ -31,14 +34,11 @@ def main():
         print('Failed to create socket')
         sys.exit()
 
-    assert host is not None
-    assert port is not None
+    assert host is not None and port is not None
 
     s.connect((host, port))
 
-    name_check = False
-    email_check = False
-    key_check = False
+    name_check = email_check = key_check = False
 
     while not name_check:
         name = 'hello|' + input('Enter your name: ').replace(' ', '_')
