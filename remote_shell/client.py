960c60f298a1907c3917c6f82ab11ac20ab13113
sebaf96 <sefernandez96@gmail.com>
2020-06-02 15:27:43 -0300

    Emprolijado codigo, agregados parametros con getopt, ahora server responde OK si el comando ejecutado no devuelve stdout, manejados algunos errores posibles

--- remote_shell/client.py
+++ remote_shell/client.py
@@ -1,16 +1,46 @@
 #!/usr/bin/python
-
+import getopt
 import socket
+import sys
+
+
+def read_options():
+    address = port = logfile_path = None
+
+    (opt, arg) = getopt.getopt(sys.argv[1:], 'a:p:l:')
+
+    if len(opt) < 2:
+        print("Error: Expected at least 2 options [-a] [-p]", len(opt), "received")
+        sys.exit(0)
+
+    for (option, argument) in opt:
+        if option == '-p':
+            port = int(argument)
+        elif option == '-a':
+            address = argument
+        elif option == '-l':
+            logfile_path = argument
+
+    assert port is not None and address is not None
+
+    return address, port, logfile_path
+
+
+def write_log(command, logfile_path):
+    from datetime import datetime
+    datetime = datetime.now().strftime("%d/%m/%Y %H:%M")
+    with open(logfile_path, 'a') as file:
+        file.write('[' + datetime + '] - ' + command + '\n')
 
 
 def main():
 
-    address, port = '127.0.0.1', 8080
+    address, port, logfile_path = read_options()
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((address, port))
-    print('Connected to remote shell on', address, 'on port', port)
-    print('Enter command exit to leave the program')
+    print('Connected to remote shell at', address, 'on port', port)
+    print("Enter command 'exit' to leave the program")
     command = ''
 
     while command != 'exit':
@@ -18,7 +48,19 @@ def main():
         s.send(command.encode('ascii'))
         answer = s.recv(4096).decode('ascii')
         print(answer)
+        write_log(command, logfile_path) if logfile_path else None
 
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except getopt.GetoptError as e:
+        print(e)
+    except AssertionError:
+        print('Expected options [-a] and [-p] specifying address and port respectively')
+    except ConnectionRefusedError:
+        print('Error: Connection refused')
+    except socket.error:
+        print('Failed to create a socket')
+    except Exception as e:
+        print(e)
--- remote_shell/server.py
+++ remote_shell/server.py
@@ -1,38 +1,62 @@
 #!/usr/bin/python
-
+import getopt
 import socket
 import subprocess as sp
+import sys
+
+
+def read_port():
+    (opt, arg) = getopt.getopt(sys.argv[1:], 'p:')
+    if len(opt) != 1:
+        raise getopt.GetoptError("Expected one option [-p] with its argument (port number)")
+
+    port = int(opt[0][1])
+    if port < 1:
+        raise ValueError
+    return port
 
 
 def main():
-    port = 8080
+    local_address = socket.gethostbyname(socket.getfqdn())
+    port = read_port()
 
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind(('', port))
+    print('Server started at', local_address, 'on port', port)
+    print('Waiting for connection...')
     server_socket.listen()
+
     client_socket, address = server_socket.accept()
+    print('Got a connection from', address[0])
 
     while True:
         command = client_socket.recv(2048)
 
         if command.decode('ascii') == 'exit':
+            client_socket.send('Bye!'.encode('ascii'))
             break
 
         with sp.Popen([command], shell=True, universal_newlines=True, stdout=sp.PIPE, stderr=sp.PIPE) as process:
-
             process_stdout, process_stderr = process.communicate()
 
-            if process.returncode is 0:
+            if process.returncode is 0 and process_stdout:
                 client_socket.send(process_stdout.encode('ascii'))
-                pass
+
+            elif process.returncode is 0 and not process_stdout:
+                client_socket.send('Ok'.encode('ascii'))
+
             else:
                 client_socket.send(process_stderr.encode('ascii'))
-                pass
 
-
-    print('Bye!')
     client_socket.close()
 
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except getopt.GetoptError as e:
+        print(e)
+    except ValueError:
+        print('Port must be a positive Integer')
+    except Exception as e:
+        print(e)
